1f2cde865c82ba4423603cd1430dd19bad5ef59c
Seema-Baghel <seemabaghel1997@gmail.com>
2020-04-25 19:56:43 +0530

    [Seema Baghel]Add. Added a program that demonstrate leap year

---
+++ leapYear.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+read -p "Enter the year:" year
+
+if(((($year%4==0)) && (($year%100!=0)) ||(($year%400==0))))
+then	
+	echo "$year is a leap year";
+else
+	echo "$year is not a leapyear";	
+fi
